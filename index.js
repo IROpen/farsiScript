4df7cd2f56ec8b404fa1a10ee1c2de6d23706f15
IROpen <hamid.6.11.79@gmail.com>
2018-11-27 22:40:48 +0330

    handle farsi syntax error

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const FSI = require('./fsinterpreter');
 const fparse = require('./fsgrammar');
 
 module.exports = {
-    eval : async x => await FSI.run(fparse(x)[0]),
+    eval : async x => {
+	try{
+	    const tr = fparse(x)[0];
+	    return await FSI.run();
+	}catch(e){
+	    return "متوجه نشدم .";
+	}
+    },
     FSI : FSI,
     fparse : fparse,
 };
