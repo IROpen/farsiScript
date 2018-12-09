82e3bed13787b426fb4ca18b10ef31532689684e
IROpen <hamid.6.11.79@gmail.com>
2018-12-09 10:48:48 +0330

    add fingilish

--- bin/console.js
+++ bin/console.js
@@ -2,6 +2,18 @@
 
 const readline = require('readline');
 const FS = require('../index');
+var program = require('commander');
+const f2f = {};
+
+program
+  .version('0.1.0')
+  .option('-f, --fingilish', 'use finglish instead of farsi')
+  .parse(process.argv);
+
+if (program.fingilish){
+    f2f.fa2fi = require('fingilish').jomle;
+}
+
 try{
     const nof = require('notify-send');
     FS.FSI.tasklist.set('چاپ',x => nof.notify('فارسی اسکریپت',x));
@@ -17,6 +29,9 @@ const rl = readline.createInterface({
 process.stdout.write('\x1b[36m>>> \x1b[0m');
 
 rl.on('line', async (input) => {
+    if (program.fingilish){
+	input = f2f.fa2fi(input);
+    }
     console.log(await FS.eval(input));
     process.stdout.write('\x1b[36m>>> \x1b[0m');
 });
