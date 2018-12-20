6e2a417fc0d599e79ccaa43e64a020a2ca686a9a
IROpen <hamid.6.11.79@gmail.com>
2018-12-20 09:44:17 +0330

    add terminal.js to dist

---
+++ docs/dist/terminal.js
@@ -0,0 +1,54 @@
+class Terminal{
+    constructor(id,options){
+	options = options || {};
+	options.fontsize = options.fontsize || '14px';
+	this.onCmd = options.onCmd;
+	this.cn = document.getElementById(id);
+	this.cn.style.wordWrap = 'break-word';
+	this.cn.style.fontsize = options.fontsize;
+	this.cn.tabIndex = '0';
+	this.cn.style.backgroundColor = 'black';
+	this.cn.style.color = 'white';
+	this.cn.style.width = '100%';
+	this.spn = document.createElement('span');
+	this.spn.style.color='cyan';
+	this.spn.innerHTML = '>>> ';
+	this.cn.appendChild(this.spn);
+	this.cq = document.createElement('input');
+	this.cq.style.font = 'inherit';
+	this.cq.style.color = 'white';
+	this.cq.style.background = 'transparent';
+	this.cq.style.outline = 'none';
+	this.cq.style.border = 'none';
+	this.cq.style.width = 'calc(100% - '+(5+this.spn.getBoundingClientRect().width)+'px)'
+	this.cn.appendChild(this.cq);
+	this.cn.onkeydown = (ke) => {
+	    if (ke.key == 'Enter'){
+		this.process(this.cq.value);
+		this.cq.value = '';
+		return;
+	    }
+	};
+    }
+    process(cmd){
+	let spn = document.createElement('span');
+	spn.style.color='cyan';
+	spn.innerHTML = '>>> ';
+	this.cn.insertBefore(spn,this.spn);
+	let txt = document.createElement('span');
+	txt.style.color = 'white';
+	txt.textContent = cmd;
+	this.cn.insertBefore(txt,this.spn);
+	this.cn.insertBefore(document.createElement('br'),this.spn);
+	if (this.onCmd){
+	    this.onCmd(cmd);
+	}
+    }
+    print(text){
+	let txt = document.createElement('span');
+	txt.style.color = 'white';
+	txt.textContent = text;
+	this.cn.insertBefore(txt,this.spn);
+	this.cn.insertBefore(document.createElement('br'),this.spn);
+    }
+};
